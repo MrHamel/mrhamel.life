ac32358225eb592aa9fbe2a68605c696dd35b5af
Ryan Hamel <ryan@rkhtech.org>
2023-07-16 17:37:48 -0700

    Copy-Pasta Markdown Blog Template

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Mr. Hamel'
+export const description = 'The Thoughts of Ryan Hamel'
+export const url = dev ? 'http://173.254.210.203:5173/' : 'https://mrhamel.life/'
